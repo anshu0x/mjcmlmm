8770b92701b3739aa6fcaee72ae65a7d69678be0
Maitrayee Das <maitrayeedas94@gmail.com>
2023-08-25 19:57:43 +0530

    first commit

---
+++ src/screens/constant/index.js
@@ -0,0 +1,197 @@
+export const STAKING_CONTRACT_ADDRESS = "0xec798dd29f9e0156ffba2a9f4fecafbd0cd725a1";
+export const STAKING_ABI = [
+    {
+        "inputs":[
+            {"internalType":"address",
+            "name":"_tokenAddress",
+            "type":"address"
+            }
+        ],
+        "stateMutability":"nonpayable",
+        "type":"constructor"
+    },
+    {
+        "anonymous":false,
+        "inputs":[
+            {
+                "indexed":true,
+                "internalType":"address",
+                "name":"buyer",
+                "type":"address"
+            },
+            {
+                "indexed":false,
+                "internalType":"uint256",
+                "name":"amount",
+                "type":"uint256"
+            },
+            {"indexed":false,
+            "internalType":"uint256",
+            "name":"tier",
+            "type":"uint256"
+        }
+    ],
+    "name":"TokenBought",
+    "type":"event"
+},
+{
+    "anonymous":false,
+    "inputs":
+    [
+        {
+            "indexed":true,
+            "internalType":"address",
+            "name":"user",
+            "type":"address"
+        },
+        {
+            "indexed":false,
+            "internalType":"uint256"
+            ,"name":"amount",
+            "type":"uint256"
+        },
+        {
+            "indexed":false,
+            "internalType":"enum Staking.Plan",
+            "name":"plan","type":"uint8"
+        },
+        {
+            "indexed":false,
+            "internalType":"uint256",
+            "name":"stakingEndTime",
+            "type":"uint256"
+        }
+    ],
+    "name":"TokensStaked",
+    "type":"event"
+},
+{
+    "anonymous":false,
+    "inputs":[
+        {
+            "indexed":true,
+            "internalType":"address",
+            "name":"user",
+            "type":"address"
+        },
+        {
+            "indexed":false,
+            "internalType":"uint256",
+            "name":"amount",
+            "type":"uint256"
+        },
+        {"indexed":false,
+        "internalType":"enum Staking.Plan",
+        "name":"plan",
+        "type":"uint8"
+    }
+],
+"name":"TokensUnstaked",
+"type":"event"
+},
+{
+    "anonymous":false,
+    "inputs":[
+        {
+            "indexed":true,
+            "internalType":"address",
+            "name":"user",
+            "type":"address"
+        },
+        {
+            "indexed":true,
+            "internalType":"address",
+            "name":"referrer",
+            "type":"address"
+        }
+    ],
+    "name":"UserReferred",
+    "type":"event"
+},
+{
+    "inputs":[],
+    "name":"FiftyUSD",
+    "outputs":[
+        {
+            "internalType":"uint256",
+            "name":"",
+            "type":"uint256"
+        }
+    ],
+    "stateMutability":"view",
+    "type":"function"
+},
+{
+    "inputs":[],
+    "name":"FiveHundreadUSD",
+    "outputs":[
+        {
+            "internalType":"uint256",
+            "name":"",
+            "type":"uint256"
+        }
+    ],
+    "stateMutability":"view",
+    "type":"function"
+},
+{
+    "inputs":[],
+    "name":"HundreadUSD",
+    "outputs":[
+        {
+            "internalType":"uint256",
+            "name":"",
+            "type":"uint256"
+        }
+    ],
+    "stateMutability":"view",
+    "type":"function"
+},
+{
+    "inputs":[],
+    "name":"ThousandUSD",
+    "outputs":[
+        {
+            "internalType":"uint256",
+            "name":"",
+            "type":"uint256"
+        }
+    ],
+    "stateMutability":"view",
+    "type":"function"
+},
+{
+    "inputs":[],
+    "name":"TwoHundreadUSD",
+    "outputs":[
+        {
+            "internalType":"uint256",
+            "name":"",
+            "type":"uint256"
+        }
+    ],
+    "stateMutability":"view",
+    "type":"function"
+},
+{
+    "inputs":[],
+    "name":"ZeroUSD",
+    "outputs":[
+        {
+            "internalType":"uint256",
+            "name":"","type":"uint256"
+        }
+    ],
+    "stateMutability":"view",
+    "type":"function"
+},
+{"inputs":[
+    {
+        "internalType":"address",
+        "name":"_referral",
+        "type":"address"}
+    ],
+    "name":"addReferral",
+    "outputs":[],
+    "stateMutability":"nonpayable",
+    "type":"function"},{"inputs":[{"internalType":"address","name":"_referrer","type":"address"},{"internalType":"uint256","name":"tokenPrice_","type":"uint256"},{"internalType":"uint256","name":"_tier","type":"uint256"}],"name":"buyTokens","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_referrer","type":"address"}],"name":"isReferred","outputs":[{"internalType":"bool","name":"","type":"bool"},{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"maxRefferalLimit","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"maxTierReferralCounts","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"ownerReferred","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"enum Staking.Plan","name":"plan","type":"uint8"},{"internalType":"uint256","name":"stakingDuration","type":"uint256"},{"internalType":"uint256","name":"teamSize","type":"uint256"}],"name":"stakeTokens","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"unstakeTokens","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"userSubscription","outputs":[{"internalType":"uint256","name":"tokenAmount","type":"uint256"},{"internalType":"uint256","name":"tier","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"users","outputs":[{"internalType":"uint256","name":"stakedAmount","type":"uint256"},{"internalType":"uint256","name":"stakingEndTime","type":"uint256"},{"internalType":"uint256","name":"teamSize","type":"uint256"},{"internalType":"enum Staking.Plan","name":"plan","type":"uint8"}],"stateMutability":"view","type":"function"}]
